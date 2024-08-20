1543b84f6d55dfd75910ceae04ffcff798b73a8f
archerzou <archer.zou84@gmail.com>
2024-08-21 00:51:56 +0800

    create account post

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,9 +1,12 @@
 import { Hono } from "hono";
 import { db } from "@/db/drizzle";
 import { and, eq, inArray } from "drizzle-orm";
-import { accounts } from "@/db/schema";
+import { accounts, insertAccountSchema } from "@/db/schema";
 
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { zValidator } from "@hono/zod-validator";
+import { createId } from "@paralleldrive/cuid2";
+import { z } from "zod";
 
 const app = new Hono()
   .get("/", clerkMiddleware(), async (c) => {
@@ -32,6 +35,35 @@ const app = new Hono()
       .from(accounts);
 
     return c.json({ data });
-  });
+  })
+  .post(
+    "/",
+    clerkMiddleware(),
+    zValidator(
+      "json",
+      insertAccountSchema.pick({
+        name: true,
+      })
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+      const values = c.req.valid("json");
+
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const [data] = await db
+        .insert(accounts)
+        .values({
+          id: createId(),
+          userId: auth.userId,
+          ...values,
+        })
+        .returning();
+
+      return c.json({ data });
+    }
+  );
 
 export default app;
--- db/schema.ts
+++ db/schema.ts
@@ -9,3 +9,5 @@ export const accounts = pgTable("accounts", {
   name: text("name").notNull(),
   userId: text("user_id").notNull(),
 });
+
+export const insertAccountSchema = createInsertSchema(accounts);
